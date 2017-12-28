4cb6ad4167a89bdf672d8f0df6aa0a86633fe6b0
KamilNowik <34374860+KamilNowik@users.noreply.github.com>
2017-12-29 00:28:14 +0100

    Zakończone

--- main.cpp
+++ main.cpp
@@ -2,8 +2,66 @@
 
 using namespace std;
 
+/////////////////////////////////////////////////////////////////////////
+
+class Ksztalt
+{
+public:
+    virtual void oblicz_pole()=0; // czysta funkcja virtualna.
+};
+
+//////////////////////////////////////////////////////////////////////////
+
+class Kolo :public Ksztalt
+{
+    float r;
+public:
+    Kolo (float x)
+    {
+       r=x;
+    }
+    virtual void oblicz_pole()
+    {
+        cout<<"Pole kola: "<<3.14*r*r<<endl;
+    }
+};
+/////////////////////////////////////////////////////////////////////////
+
+class Kwadrat :public Ksztalt
+{
+    float a;
+public:
+    Kwadrat(float x)
+    {
+        a=x;
+    }
+    virtual void oblicz_pole()
+    {
+        cout<<"Pole kwadratu = "<<a*a<<endl;
+    }
+};
+////////////////////////////////////////////////////////////////////////
+
+void obliczenia(Ksztalt *x)
+{
+    x->oblicz_pole();
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+
+    Kolo k(1);
+    Kwadrat kw(2);
+
+    Ksztalt *wsk;
+
+    wsk =&k;
+    wsk ->oblicz_pole();
+
+    wsk =&kw;
+    wsk ->oblicz_pole();
+
+    obliczenia(wsk);
+
     return 0;
 }
